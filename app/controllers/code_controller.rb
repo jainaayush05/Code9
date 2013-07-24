b54f1b6d172817404adee9fc4a572ec6af9e1c96
Aayush <jainaayush05@gmail.com>
2013-07-24 21:26:55 +0530

    Doc list now displaying doc code

--- app/controllers/code_controller.rb
+++ app/controllers/code_controller.rb
@@ -1,9 +1,20 @@
 class CodeController < ApplicationController
 require 'securerandom'
 def index
-	redirect_to "http://localhost:3000/#{SecureRandom.hex(2)}"
+	@rand_str=SecureRandom.hex(2);
+	redirect_to "http://localhost:3000/#{@rand_str}"
 end
 def show
+	@cookies =[cookies[:r1],cookies[:r2],cookies[:r3],cookies[:r4],cookies[:r5]]
+	hash = Hash[@cookies.map.with_index.to_a]
+	hash["#{params[:id]}"]
+	if !@cookies.include? ("#{params[:id]}")
+	cookies[:r5]=cookies[:r4]
+	cookies[:r4]=cookies[:r3]
+	cookies[:r3]=cookies[:r2]
+	cookies[:r2]=cookies[:r1]
+	cookies[:r1]={ :value => "#{params[:id]}"}
+	end
 	render('show')
 end
 def full
